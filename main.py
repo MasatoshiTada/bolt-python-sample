95b44dcbbea3b68897b737c022cb84a7a99b743c
Masatoshi Tada <tada@casareal.co.jp>
2020-10-14 15:40:39 +0900

    add message_hello

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ def slack_events():
     return handler.handle(request)
 
 
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+
 # heroku login
 # heroku create
 # git remote add heroku https://git.heroku.com/xxx.git
