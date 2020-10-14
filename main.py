9c9f6a7367229ca8c372bc8de496b52727a1a363
Masatoshi Tada <tada@casareal.co.jp>
2020-10-14 16:32:27 +0900

    add open_modal

--- main.py
+++ main.py
@@ -50,6 +50,46 @@ def action_button_click(body, ack, say):
     say(f"<@{body['user']['id']}> clicked the button")
 
 
+# Listen for a shortcut invocation
+@app.shortcut("open_modal")
+def open_modal(ack, body, client):
+    # Acknowledge the command request
+    ack();
+    # Call views_open with the built-in client
+    client.views_open(
+        # Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id=body["trigger_id"],
+        # View payload
+        view={
+            "type": "modal",
+            # View identifier
+            "callback_id": "view_1",
+            "title": {"type": "plain_text", "text": "My App"},
+            "submit": {"type": "plain_text", "text": "Submit"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": "Welcome to a modal with _blocks_"},
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Click me!"},
+                        "action_id": "button_abc"
+                    }
+                },
+                {
+                    "type": "input",
+                    "block_id": "input_c",
+                    "label": {"type": "plain_text", "text": "What are your hopes and dreams?"},
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "dreamy_input",
+                        "multiline": True
+                    }
+                }
+            ]
+        }
+    )
+
 # heroku login
 # heroku create
 # git remote add heroku https://git.heroku.com/tada-bolt-python-sample.git
